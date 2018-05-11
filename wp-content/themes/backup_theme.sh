886afe2523074beaf69108af9c628c307811eb82
Levi D. Smith <me@levidsmith.com>
2018-05-11 12:26:57 -0700

    Latest website updates

--- wp-content/themes/backup_theme.sh
+++ wp-content/themes/backup_theme.sh
@@ -1,3 +1,3 @@
-FILENAME="twentyseventeen-child-"$(date +"%Y%m%d")".tar.gz"
+FILENAME="backups/twentyseventeen-child-"$(date +"%Y%m%d")".tar.gz"
 echo $FILENAME
 tar -cvzf $FILENAME twentyseventeen-child
